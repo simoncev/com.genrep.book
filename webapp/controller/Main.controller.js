7a92e22a3f184bc329106d2f6e74acb2d9a4853f
simoncev <simoncev@gmail.com>
2024-01-11 13:48:41 +0100

    Message Box test

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -2,6 +2,28 @@ sap.ui.define(["./BaseController", "sap/m/MessageBox"], function (BaseController
 	"use strict";
 
 	return BaseController.extend("com.genrep.book.controller.Main", {
+		onInit: function () {
+			this._myDelegate = {
+				onmouseover: this._showPopover,
+				onmouseout: this._clearPopover,
+			};
+			this.byId("idAuthor").addEventDelegate(this._myDelegate, this);
+		},
+		_showPopover: function () {
+			this._timeId = setTimeout(() => {
+				MessageBox.show("Message on Mouse Over");
+			}, 500);
+			
+		  },
+	  
+		  _clearPopover: function () {
+			clearTimeout(this._timeId);
+		  },
+	  
+		  onExit: function() {
+			this._clearPopover();
+			this.byId("idAuthor").removeEventDelegate(this._myDelegate);
+		  },
 		onPlayVideo: function () {
 			MessageBox.show("Open Page to Play Video");
 		}
